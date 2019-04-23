4cdb8442ebe87bcb8d3196e6c0b841738d9db52d
Maxence GRAB <magrab@e1r4p9.42.fr>
2019-04-23 15:58:09 +0200

    Add texture to struct

--- srcs/wolf_init.c
+++ srcs/wolf_init.c
@@ -17,6 +17,23 @@ static void		void_wolf(t_wolf *wolf)
 	ft_bzero(wolf, sizeof(t_wolf));
 }
 
+static SDL_Surface	*load_texture(t_wolf *wolf, const char *file)
+{
+	SDL_RWops *rwop;
+	SDL_Surface *surf;
+
+	rwop = SDL_RWFromFile(file, "rb");
+	if(!(surf = IMG_LoadXPM_RW(rwop)))
+	{
+		ft_printf("IMG_LoadXPM_RW: %s\n", IMG_GetError());
+		SDL_RWclose(rwop);
+		prog_quit(wolf);
+		return (NULL);
+	}
+	SDL_RWclose(rwop);
+	return (surf);
+}
+
 t_wolf			*wolf_init(void)
 {
 	t_wolf *wolf;
@@ -35,6 +52,10 @@ t_wolf			*wolf_init(void)
 	wolf->sdl.btnmap[1] = add_mapmenu_button(wolf);
 	wolf->sdl.btnopt[0] = wolf->sdl.btnmap[0];
 	wolf->sdl.btnopt[1] = wolf->sdl.btnarr[0];
+	wolf->wl_txture[0] = load_texture(wolf, "./ressources/textures/wall_blue.xpm");
+	wolf->wl_txture[1] = load_texture(wolf, "./ressources/textures/wall.xpm");
+	wolf->wl_txture[2] = load_texture(wolf, "./ressources/textures/wall_blue.xpm");
+	wolf->wl_txture[3] = load_texture(wolf, "./ressources/textures/wall.xpm");
 	wolf->pos.x = 0;
 	wolf->pos.y = 0;
 	wolf->fov = 60;
