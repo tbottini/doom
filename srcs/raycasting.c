2c7551c5650bf3abc28691233f07643f32eb2bb2
tbottini <thomasbottini@protonmail.com>
2019-04-24 21:19:10 +0200

    Makefile, raycasting mur droit, texture 0.1
    Former-commit-id: 558c5492c44fcfacc3fe223b133f553ffb78b94a [formerly d44a55995e5acdd99d6c5a6035937997d73ba7ba]
    Former-commit-id: 485efc1df8883acfb1cd83fca31c3e69f1c298ad

--- srcs/raycasting.c
+++ srcs/raycasting.c
@@ -12,61 +12,26 @@ double		iswall(t_wolf *wolf, t_fvct2 inter)
 	return (wolf->map_size.x + wolf->map_size.y);
 }
 
-void		raycasting(t_wolf *wolf)
-{
-	t_ray	ray;
-	float	iangle;
-	int		i;
+ void		raycasting(t_wolf *wolf)
+ {
+ 	t_ray	ray;
+ 	int		i;
 
-	i = 0;
-	//ray.angle = wolf->rot + wolf->fov / 2;
-	//iangle = wolf->fov / (float)wolf->sdl.size.x;
-
-	wolf->d_scrn = (wolf->sdl.size.x / 2.0) / tan(wolf->fov * PI180 / 2.0);
-
-
-
-	while (i < wolf->sdl.size.x)
-	{
-		ray.angle = atan(((wolf->sdl.size.x / 2.0) - i) / wolf->d_scrn) * 180.0 / 3.14159;
-		ray.angle += wolf->rot;
-		ray.angle = angle_adaptater(ray.angle);
-		if (i % 100 == 0)
-			printf("i %d angle %f\n", i, ray.angle);
-		//ray.angle = angle_adaptater(ray.angle);
-		hor_detection(wolf, &ray);
-		ver_detection(wolf, &ray);
-		draw_column(wolf, ((ray.hor < ray.ver) ? ray.hor : ray.ver), i, ray.angle);
-		//ray.angle -= iangle;
-		i++;
-	}
-	SDL_RenderCopy(wolf->sdl.rend, wolf->sdl.txture, NULL, NULL);
-	SDL_RenderPresent(wolf->sdl.rend);
-}
-
-void		raythrowing_debug(t_wolf *wolf)
-{
-	t_ray	ray;
-	float	iangle;
-	int		i;
-	float	sx;
-
-	i = 0;
-	ray.angle = wolf->rot + wolf->fov / 2;
-	iangle = wolf->fov / (float)wolf->sdl.size.x;
-	sx =  (wolf->sdl.size.x / 2) * (tan(wolf->fov / 2));
-	while (i < wolf->sdl.size.x)
-	{
-		//ray.angle = angle_adaptater(atan((i - (wolf->sdl.size.x / 2)) / sx));
-		//ray.angle = 1;
-		ray.angle = angle_adaptater(ray.angle);
-		hor_detection(wolf, &ray);
-		ver_detection(wolf, &ray);
-		if (i > 1590 && i < 1595)
-		{
-			printf("i %d dist %f\n", i, (ray.hor < ray.ver ? ray.hor : ray.ver));
-		}
-		ray.angle -= iangle;
-		i++;
-	}
+ 	i = 0;
+ 	//ray.angle = wolf->rot + wolf->fov / 2;
+ 	//iangle = wolf->fov / (float)wolf->sdl.size.x;
+ 	wolf->d_scrn = (wolf->sdl.size.x / 2.0) / tan(wolf->fov * PI180 / 2.0);
+ 	while (i < wolf->sdl.size.x - 1)
+ 	{
+ 		ray.angle = atan(((wolf->sdl.size.x / 2.0) - i) / wolf->d_scrn) * TOANGLE;
+ 		ray.angle += wolf->rot;
+ 		ray.angle = angle_adaptater(ray.angle);
+ 		hor_detection(wolf, &ray);
+ 		ver_detection(wolf, &ray);
+ 		draw_column(wolf, ray, i);
+ 		//ray.angle -= iangle;
+ 		i++;
+ 	}
+ 	SDL_RenderCopy(wolf->sdl.rend, wolf->sdl.txture, NULL, NULL);
+ 	SDL_RenderPresent(wolf->sdl.rend);
 }
