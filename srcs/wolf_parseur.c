16d5acadc126bb6f108ed8fd39f21e56d9148a61
tbottini <thomasbottini@protonmail.com>
2019-04-25 22:36:01 +0200

    list file parsing
    Former-commit-id: 578f541ebba87b0f7d60e4e0dabe32b1b41f1390
    Former-commit-id: 054c6968fdb39ffdcf91ebcfbcfd958c497bdf26

--- srcs/wolf_parseur.c
+++ srcs/wolf_parseur.c
@@ -6,7 +6,7 @@
 /*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 20:53:30 by tbottini          #+#    #+#             */
-/*   Updated: 2019/04/25 20:55:19 by tbottini         ###   ########.fr       */
+/*   Updated: 2019/04/25 22:32:06 by tbottini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,60 +19,60 @@ int				row_verif(t_wolf *wolf, char *row)
 	i = 0;
 	while (row[i])
 	{
-		if (row[i] == 'A')
+		if (row[i] == 'A' && (int)wolf->pos.x == 0)
 		{
 			wolf->pos.x = i + 0.5;
 			wolf->pos.y = (double)wolf->map_size.y + 0.5;
 		}
-		else if (row[i] != '.' && row[i] != '#')
+		else if (row[i] == 'A' || (row[i] != '.' && row[i] != '#'))
 			return (0);
 		i++;
 	}
+	if ((wolf->map_size.x != 0 && wolf->map_size.x != i) || !i)
+		return (0);
+	else
+		wolf->map_size.x = i;
 	return (1);
 }
 
-int				map_verif(int fd, t_wolf *wolf)
+t_list			*get_file_lst(t_wolf *wolf, int fd)
 {
-	char		*line;
 	int			ret;
-	int			flag;
+	char		*line;
+	t_list		*file;
+	t_list		*new;
 
-	line = NULL;
-	flag = 0;
-	wolf->map_size.x = 0;
 	wolf->map_size.y = 0;
-	while (flag == 0 && (ret = get_next_line(fd, &line)) > 0)
+	wolf->map_size.x = 0;
+	wolf->pos.x = 0;
+	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		if (wolf->map_size.x == 0)
-		{
-			if (ft_strlen(line) < 100)
-				wolf->map_size.x = (int)ft_strlen(line);
-			else
-				return (0);
-		}
-		if (ft_strcmp(line, "[Textures]") == 0)
-			flag = 1;
-		else if ((int)ft_strlen(line) == wolf->map_size.x)
-		{
-			if (row_verif(wolf, line))
-				ft_strcpy(wolf->map[wolf->map_size.y], line);
-			else
-				return (0);
-		}
+		new = ft_lstnew(NULL, 0);
+		new->content = line;
+		if (!new)
+			return (NULL);
+		if (row_verif(wolf, line))
+			ft_lstadd(&file, new);
 		else
-			return (0);
+			return (NULL);
 		wolf->map_size.y++;
-		free(line);
+		printf("line : %s\n", line);
 	}
-	return (1);
+	return ((ret == 0) ? file : NULL);
 }
 
 int				wolf_parseur(t_wolf *wolf, char *filename)
 {
 	int			fd;
+	t_list		*file;
 
+	printf("new_file\n");
 	fd = open(filename, O_RDONLY);
-	if (map_verif(fd, wolf) == 0)
+	if (!(file = get_file_lst(wolf, fd)))
+	{
+		printf("mauvais fichier\n\n");
 		return (0);
+	}
+	printf("bon fichier w %d h %d\n", wolf->map_size.x, wolf->map_size.y);
 	return (1);
 }
