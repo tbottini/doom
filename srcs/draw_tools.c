cfa831be2507e32a8f3ab15901a7279e15cc3f3b
tbottini <thomasbottini@protonmail.com>
2019-04-25 18:57:41 +0200

    nouvelle structure pour les murs

--- srcs/draw_tools.c
+++ srcs/draw_tools.c
@@ -35,20 +35,20 @@ void		draw_part_texture(t_wolf *wolf, t_ray ray, int *istart, int length)
 
 	ray_polarity(&ray);
 	ctexture.x = (ray.hor < ray.ver) ? ray.inter_h.x : ray.inter_v.y;
-	ctexture.x = (ctexture.x - (int)ctexture.x) * (wolf->wl_txture[ray.polar]->w);
+	ctexture.x = (ctexture.x - (int)ctexture.x) * (wolf->nwall[ray.polar].w);
 	ctexture.y = 0;
-	dty = (wolf->wl_txture[ray.polar]->h) / (float)length;
+	dty = (wolf->nwall[ray.polar].h) / (float)length;
 	if (length > wolf->sdl.size.y)
 	{
 		ctexture.y = (((float)length - (float)wolf->sdl.size.y) / 2);
-		ctexture.y = ctexture.y / (float)length * wolf->wl_txture[ray.polar]->h;
+		ctexture.y = ctexture.y / (float)length * wolf->nwall[ray.polar].h;
 		length = wolf->sdl.size.y - 1;
 	}
 	i = 0;
 	while (i < length)
 	{
-		it = (int)ctexture.x + (int)ctexture.y * wolf->wl_txture[ray.polar]->w;
-		wolf->sdl.screen[*istart] = wolf->wall[ray.polar][it];
+		it = (int)ctexture.x + (int)ctexture.y * wolf->nwall[ray.polar].w;
+		wolf->sdl.screen[*istart] = wolf->nwall[ray.polar].txture[it];
 		ctexture.y += dty;
 		*istart += wolf->sdl.size.x;
 		i++;
