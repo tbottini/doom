861a6fffe3e11972b364a52a8ac43c5c56aeb61b
tbottini <thomasbottini@protonmail.com>
2019-04-26 16:03:56 +0200

    mur invisible, compilation libft, fin de parsing
    Former-commit-id: cc2e096ad1b78231e6b1f3e75f81685f6193e5d3
    Former-commit-id: 1f4a26f04934096cb97ea1e2daee018a24e75103

--- srcs/wolf_parseur.c
+++ srcs/wolf_parseur.c
@@ -6,7 +6,7 @@
 /*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 20:53:30 by tbottini          #+#    #+#             */
-/*   Updated: 2019/04/25 22:46:39 by tbottini         ###   ########.fr       */
+/*   Updated: 2019/04/26 13:28:27 by tbottini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,37 @@ t_list			*get_file_lst(t_wolf *wolf, int fd)
 		else
 			return (NULL);
 		wolf->map_size.y++;
-		printf("line : %s\n", line);
 	}
+	if (wolf->pos.x == 0)
+		return (0);
 	return ((ret == 0) ? file : NULL);
+}
+
+int			list_to_map(t_wolf *wolf, t_list **f)
+{
+	int			row;
+	t_list		*tmp;
+	t_list		*file;
+
+	file = *f;
+	wolf->map = (char **)malloc(sizeof(char *) * (wolf->map_size.y));
+	if (!wolf->map)
+		return (0);
+	row = wolf->map_size.y - 1;
+	while (row > -1)
+	{
+		wolf->map[row] = (char *)malloc(sizeof(char *) * (wolf->map_size.x + 1));
+		if (!wolf->map[row])
+			return (0);
+		ft_putendl((char *)file->content);
+		ft_strcpy(wolf->map[row], (char *)file->content);
+		tmp = file;
+		file = file->next;
+		free(tmp->content);
+		free(tmp);
+		row--;
+	}
+	return (1);
 }
 
 int				wolf_parseur(t_wolf *wolf, char *filename)
@@ -73,6 +101,16 @@ int				wolf_parseur(t_wolf *wolf, char *filename)
 		printf("mauvais fichier\n\n");
 		return (0);
 	}
-	printf("bon fichier w %d h %d\n", wolf->map_size.x, wolf->map_size.y);
+	if (!list_to_map(wolf, &file))
+		return (0);
+
+	int i;
+	i = 0;
+	printf("%s: bonne map\nmap size w %d h %d\njoueur pos .x %f .y %f\n", filename, wolf->map_size.x, wolf->map_size.y, wolf->pos.x, wolf->pos.y);
+	while (i < wolf->map_size.y)
+	{
+		ft_putendl(wolf->map[i]);
+		i++;
+	}
 	return (1);
 }
