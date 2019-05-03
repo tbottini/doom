a2a4fc00ae100c5974ee183d07ec595edb1a4fd9
tbottini <thomasbottini@protonmail.com>
2019-05-03 21:45:51 +0200

    start bunch construction and comsuption

--- srcs/num_tools.c
+++ srcs/num_tools.c
@@ -6,7 +6,7 @@
 /*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 19:36:06 by tbottini          #+#    #+#             */
-/*   Updated: 2019/05/03 12:59:54 by tbottini         ###   ########.fr       */
+/*   Updated: 2019/05/03 13:18:01 by tbottini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,58 @@ double		angle_adaptater(double angle)
 	else if (angle > 360.0)
 		angle = double_modulo(angle);
 	return (angle);
+}
+#include "doom_nukem.h"
+
+float		ft_atof(char *str)
+{
+	float	nb;
+	int		i;
+	int		sign;
+	float	j;
+
+	j = 10.0;
+	sign = (str[0] == '-') ? -1 : 1;
+	nb = 0.0;
+	i = 0;
+	while (str[i] && str[i] != '.')
+	{
+		nb = nb * 10 + str[i] - '0';
+		i++;
+	}
+	i++;
+	while (str[i])
+	{
+		nb = nb + (str[i] - '0') / j;
+		j *= 10.0;
+		i++;
+	}
+	return (nb);
+}
+
+float		ft_catof(char *str, char c)
+{
+	float	nb;
+	int		i;
+	int		sign;
+	float	j;
+
+	j = 10.0;
+	sign = (str[0] == '-') ? -1 : 1;
+	nb = 0.0;
+	i = 0;
+	while (str[i] && str[i] != '.' && str[i] != c)
+	{
+		nb = nb * 10 + str[i] - '0';
+		i++;
+	}
+	if (str[i] != ' ')
+		i++;
+	while (str[i] && str[i] != c)
+	{
+		nb = nb + (str[i] - '0') / j;
+		j *= 10.0;
+		i++;
+	}
+	return (nb);
 }
