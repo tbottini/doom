3582fd1bbbfaf9712e7cabcba340b57e60688f42
Armand KRACHE <Armandkrache@gmail.com>
2019-05-06 18:20:27 +0200

    Ajout foncitons inputs
    Former-commit-id: 15e491b8b7e44b19b21f933dba51c77bf6ba69a2 [formerly 9a42917decd7000b1529b7a476d5008de32e2f82]
    Former-commit-id: 317d6449520d3538327cc03ab961a81de7962ef1

---
+++ srcs/input/move.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akrache <akrache@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/06 15:13:17 by akrache           #+#    #+#             */
+/*   Updated: 2019/05/06 15:17:55 by akrache          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "doom_nukem.h"
+
+void		crouch(t_doom *doom)
+{
+
+	if (!(tab->player->crouch))
+	{
+		tab->player->crouch = 1;
+		tab->player->speed >>= 1
+		tab->player->height >>= 1
+	}
+}
+
+void		crouch_release(t_doom *doom)
+{
+
+	tab->player->crouch = 0;
+	tab->player->speed >>= 1;
+	tab->player->height >>= 1;
+}
+
+void		sprint(t_doom *doom)
+{
+	if (doom->player->speed < MAX_SPEED)
+		doom->player->speed += 0.2;
+}
+
+void		sprint_release(t_doom *doom)
+{
+	doom->player->speed = 10;
+}
+
+void		move(t_doom *doom, int x, int y)
+{
+	t_fvct2 d;
+	t_fvct2 npos;
+
+	// Update Rotation
+	doom->player.rot.y += 5.0;
+	if (doom->player.rot.y < 0.0)
+		doom->player.rot.y += 360.0;
+	else if (doom->player.rot.y > 360)
+		doom->player.rot.y -= 360.0;
+	// Update Position
+	d.x = sin(doom->player.rot.y * PI180) / 10.0;
+	d.y = cos(doom->player.rot.y * PI180) / 10.0;
+	npos.x = doom->player.pos.x + d.x * -y / 35000.0 + d.y * 1 * -x / 35000.0;
+	npos.y = doom->player.pos.y + d.x * -x / 35000.0 + d.y * y / 35000.0;
+	// Check new pos
+	doom->player.pos = npos;
+}
