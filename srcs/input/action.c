8f08670e1bf6b6f5d2c86f63b79753d4c2b2675a
Armand KRACHE <Armandkrache@gmail.com>
2019-05-08 19:04:44 +0200

    ajout fonctions dans le code

--- srcs/input/action.c
+++ srcs/input/action.c
@@ -6,7 +6,7 @@
 /*   By: akrache <akrache@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 15:35:25 by akrache           #+#    #+#             */
-/*   Updated: 2019/05/06 15:40:34 by akrache          ###   ########.fr       */
+/*   Updated: 2019/05/08 17:16:16 by akrache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ void		prev_weapon(t_player *player)
 
 void		action(t_doom *doom)
 {
-	;
+	if (is_in_range(player, sector))
+		interact(sector);
 }
 
 int			is_in_range(t_player *player, t_sector *sector)
