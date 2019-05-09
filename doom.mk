b7e4fe7d0ade36a4db9bff1bb86ef39b89feefdf
Armand KRACHE <Armandkrache@gmail.com>
2019-05-09 19:57:17 +0200

    ajout fonction opacity et debut minimap
    Former-commit-id: 21ccddaeea3276f098078b7935d35189b9425895
    Former-commit-id: b8b8b067dce89093566f995eb66012eb853eca1a

--- doom.mk
+++ doom.mk
@@ -17,6 +17,9 @@ SRCS_INPUT		:=		event_controller.c								\
 						event_handler2.c								\
 						input_hook.c									\
 						loop_hook.c										\
+						action.c										\
+						move.c											\
+						shoot.c											\
 						keyboard_input.c
 
 SRCS_RENDER		:=		bunch.c											\
@@ -24,6 +27,7 @@ SRCS_RENDER		:=		bunch.c											\
 						bresenham.c										\
 						draw_wall.c										\
 						sector.c
+						minimap.c
 
 SRCS_PARSING	:=		parsing.c										\
 						chunck.c
--- srcs/input/shoot.c
+++ srcs/input/shoot.c
@@ -6,40 +6,43 @@
 /*   By: akrache <akrache@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 19:51:14 by akrache           #+#    #+#             */
-/*   Updated: 2019/05/06 15:45:37 by akrache          ###   ########.fr       */
+/*   Updated: 2019/05/08 19:21:43 by akrache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom_nukem.h"
 
+void		send_bullet(t_fvct2 pos, t_texture *tex)
+{
+	;
+}
+
 void		reload(t_weapon *weapon)
 {
 	int	r;
 
-	r = clip_max - clip;
-	(weapon.ammo - r < 0 ? r += weapon.ammo - r : 0);
-	weapon.ammo -= r;
-	weapon.clip = r;
+	r = weapon->clip_max - weapon->clip;
+	(weapon->ammo - r < 0 ? r += weapon->ammo - r : 0);
+	weapon->ammo -= r;
+	weapon->clip = r;
 }
 
 void		shoot(t_doom *doom)
 {
-	if (doom->player.weapons[hand].clip == 0)
-		reload(weapon);
+	if (doom->player.weapons[doom->player.hand].clip == 0)
+		reload(&(doom->player.weapons[doom->player.hand]));
 	else
 	{
-		if (doom->player.hand == ROCKETLAUNCHER)
-			send_bullet(player.pos, /*RocketTexture*/);
+		if (doom->player.hand == 4)//ROCKETLAUNCHER)
+			;//send_bullet(player.pos, /*RocketTexture*/);
 		else
-			send_bullet(player.pos, 0);
-		clip--;
+			send_bullet(doom->player.pos, 0);
+		doom->player.weapons[doom->player.hand].clip--;
 	}
 }
 
 void		kick(t_player *player, t_sector *sector)
 {
-	if (is_in_range(player, sector) && is_ennemy(sector))
-		sector->health -= player->dmg;
+	;//if (is_in_range(player, sector) && is_ennemy(sector))
+	//	sector->health -= player->dmg;
 }
-
-void		send_bullet(t_fvct2 pos, t_texture *tex);
