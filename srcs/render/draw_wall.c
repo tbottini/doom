22d7e2e4bdb2ad36291942c3b15e1e527078a749
tbottini <thomasbottini@protonmail.com>
2019-05-13 18:53:26 +0200

    clipping debug, pillar info(px, dist)
    Former-commit-id: 76e21a1debc4f5df3e6e42112104d827a5cda77c [formerly f517c23b13b83b96dcfeab138e0bb579b35bb233]
    Former-commit-id: 94baefaf4ddbb9795e9e3512ed7edd954c1d2742

--- srcs/render/draw_wall.c
+++ srcs/render/draw_wall.c
@@ -13,36 +13,67 @@ print un wall il faut
 -faire un degrader jusqu'au pillier
 */
 
-//int			pillar_info()
-//{
-//	if (bunch[i]->frust && !bunch[i + 1]->frust)
-//	{
-//		px = doom->sdl.size.x - 1;
-//		dist = wall_clipping(*bunch[i], *bunch[i + 1], player.pos, player.rot.y + player.fov / 2.0);
-//	}
-//	else if (bunch[i + 1]->frust && !bunch[i]->frust)
-//	{
-//		px = 0;
-//		dist = wall_clipping(*bunch[i], *bunch[i + 1], player.pos, double_modulo(player.rot.y - player.fov / 2.0));
-//	}
-//	else
-//	{
-//		px = ((float)(doom->sdl.size.x - 1) / 2.0) - (float)(doom->sdl.size.x) / player.fov * bunch[i]->angle;
-//		dist = distance(player.pos, bunch[i]->pillar);
-//	}
-//}
-//
-//void		draw_part(t_sdl *sdl, t_wall, int *i)
-//{
-//
-//}
-//
-//
-//void		draw_wall(t_doom doom , t_wall wall)
-//{
-//	t_fvct2	dist;
-//	t_vct2	px;
-//	int		i;
-//
-//	i = get_column_id()
-//}
+//il faut definir pillar et *next
+//		une verification pa
+
+//		si on est endehors du frustrum
+
+
+void		pillar_screen_info(t_doom doom, t_wall wall, t_fvct2 *dist, t_vct2 *column_id)
+{
+	t_vct2 px;
+	t_fvct2 d;
+	t_player	*p;
+
+	p = &doom.player;
+	if (wall.pillar.frust)
+	{
+		px.x = (float)(doom.sdl.size.x) / 2.0;
+		px.x -= (float)(doom.sdl.size.x - 1) / doom.player.fov * wall.pillar.angle;
+		d.x = distance(p->pos, wall.pillar.p);
+	}
+	else if (wall.pillar.angle <= -doom.player.fov / 2.0)
+	{
+		px.x = doom.sdl.size.x - 1;
+		d.x = wall_clipping(wall, p->pos, p->rot.y - p->fov / 2.0);
+	}
+	else if (wall.pillar.angle >= doom.player.fov / 2.0)
+	{
+		px.x = 0;
+		d.x = wall_clipping(wall, p->pos, p->rot.y + p->fov / 2.0);
+	}
+	if (wall.next->frust)
+	{
+		px.y = (float)(doom.sdl.size.x) / 2.0;
+		px.y -= (float)(doom.sdl.size.x - 1) / doom.player.fov * wall.next->angle;
+		d.y = distance(p->pos, wall.next->p);
+	}
+	else if (wall.next->angle <= -doom.player.fov / 2.0)
+	{
+		px.y = doom.sdl.size.x - 1;
+		d.y = wall_clipping(wall, p->pos, p->rot.y - p->fov / 2.0);
+	}
+	else if (wall.next->angle >= doom.player.fov / 2.0)
+	{
+		px.y = 0;
+		d.y = wall_clipping(wall, p->pos, p->rot.y + p->fov / 2.0);
+	}
+	*column_id = px;
+	*dist = d;
+}
+
+void		draw_wall(t_doom doom , t_wall wall)
+{
+	t_vct2	column_id;
+	t_fvct2	dist;
+
+	//test mur 1
+	printf("--------------------test_mur----------------------------\n");
+	fvct2_msg("pos pillar", wall.pillar.p);
+	fvct2_msg("pos next", wall.next->p);
+	pillar_screen_info(doom, wall, &dist, &column_id);
+	printf("px pillar %d next %d\n dist pillar %f next %f\n", column_id.x, column_id.y, dist.x, dist.y);
+	printf("--------------------------------------------------------\n");
+	//printf("pillar column id %d dist %f\n", column_id.x, dist.x);
+	//printf("pillar2 column id %d dist %f\n", column_id.y, dist.y);
+}
