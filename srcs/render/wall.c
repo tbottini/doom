bf18f05d675c2dd3ca6d765212c143bdb023b240
tbottini <thomasbottini@protonmail.com>
2019-05-15 22:07:44 +0200

    visible wall with out frustum pillar
    Former-commit-id: 6bad5cfd86c4874064b314bb822724e377509bc9 [formerly 38516b79ab7c294b926e127a3855e64f2e2f8526]
    Former-commit-id: c3d33e7b5b38ca6e8bd501282b22b3c6426e0260

--- srcs/render/wall.c
+++ srcs/render/wall.c
@@ -17,7 +17,7 @@ float			wall_clipping(t_wall wall, t_fvct2 pos, float angle)
 	diff.y = wall.pillar.p.y - pos.y;
 	diff2.x = wall.next->p.x - pos.x;
 	diff2.y = wall.next->p.y - pos.y;
-	coef_ang = tan(angle * PI180);;
+	coef_ang = tan(angle * PI180);
 	if (diff2.x - diff.x < 0.001 && diff2.x - diff.x > -0.001)
 	{
 		inter.x = diff.x;
@@ -26,7 +26,7 @@ float			wall_clipping(t_wall wall, t_fvct2 pos, float angle)
 	else
 	{
 		coef_wall = (diff2.y - diff.y) / (diff2.x - diff.x);
-		b = diff.x * coef_wall + diff.y;
+		b = diff.y - diff.x * coef_wall;
 		inter.x = b / (coef_ang - coef_wall);
 		inter.y = coef_wall * inter.x + b;
 	}
