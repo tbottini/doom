4971f90bf16ce7a0ebb4a4870288246366801cf8
tbottini <thomasbottini@protonmail.com>
2019-05-16 17:15:25 +0200

    remove movement, rectif fonction

---
+++ include/debug.h
@@ -0,0 +1,14 @@
+
+#ifndef DEBUG_H
+# define DEBUG_H
+
+#include "libft.h"
+
+t_wall					*chunck_walls(t_list *chunck_line, size_t len);
+t_player				chunck_player(int fd);
+t_sector				*chunck_sector(int fd);
+t_fvct2					*double_atof(char *line, t_fvct2 *vct);
+t_sector				*sector_new();
+t_list					*ft_lstn(void *content);
+
+#endif
