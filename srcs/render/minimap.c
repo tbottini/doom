99c3883c5f7e5f03b49fc24e7fb3dca91e045dfa
Armand KRACHE <Armandkrache@gmail.com>
2019-05-16 17:02:02 +0200

    push minimap
    Former-commit-id: 5452eaf9be25ed22e7f93809df2c8a33108e2877
    Former-commit-id: 7c085987195dcf205da67729da178d61484b288e

--- srcs/render/minimap.c
+++ srcs/render/minimap.c
@@ -6,7 +6,7 @@
 /*   By: akrache <akrache@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 16:13:54 by akrache           #+#    #+#             */
-/*   Updated: 2019/05/14 22:55:30 by akrache          ###   ########.fr       */
+/*   Updated: 2019/05/16 14:48:10 by akrache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,109 +20,59 @@ typedef struct			s_minimap
 	t_vct2	mid;
 }						t_minimap;
 
-#define UNIT 20.0
-#define CWALL 0xFFFF00FF
-#define CPERS 0x0C1597FF
-#define WHITE 0xFFFFFFFF
+# define UNIT 20.0
+# define CWALL 0xDADADAFF
+# define CPERS 0xFFFFFFFF
+# define WHITE 0xFFFFFFFF
 
+/*
+void		input_deplacement(t_doom *doom, int key)
+{
+	double x_dir;
+	double y_dir;
 
-void		bold_point2(t_vct2 v, uint32_t color, t_doom *doom, t_minimap mini)
+	y_dir = cos(doom->player.rot.y * PI / 180) * 15;
+	x_dir = sin(doom->player.rot.y * PI / 180) * 15;
+	if (key == SDLK_w)
+	{
+		doom->player.pos.y += y_dir;
+		doom->player.pos.x -= x_dir;
+	}
+	else if (key == SDLK_a)
+	{
+		doom->player.pos.y -= x_dir;
+		doom->player.pos.x -= y_dir;
+	}
+	else if (key == SDLK_s)
+	{
+		doom->player.pos.y -= y_dir;
+		doom->player.pos.x += x_dir;
+	}
+	else if (key == SDLK_d)
+	{
+		doom->player.pos.y += x_dir;
+		doom->player.pos.x += y_dir;
+	}
+}*/
+
+int			bold_point2(t_doom *doom, t_minimap mini, t_vct2 pos, Uint32 color)
 {
 	int tmp;
 
-	tmp = v.y * doom->sdl.size.x;
-	if (v.y > mini.d.y && v.y < mini.a.y - 1 && v.x < mini.a.x - 1 && v.x > mini.d.x)
+	tmp = pos.y * doom->sdl.size.x;
+	if (pos.y > mini.d.y && pos.y < mini.a.y - 1 && pos.x < mini.a.x - 1 && pos.x > mini.d.x)
 	{
-		doom->sdl.screen[v.x + tmp] = color;
-		doom->sdl.screen[v.x + 1 + tmp] = color;
-		doom->sdl.screen[v.x - 1 + tmp] = color;
-		doom->sdl.screen[v.x + 1 + tmp + doom->sdl.size.x] = color;
-		doom->sdl.screen[v.x - 1 + tmp + doom->sdl.size.x] = color;
-		doom->sdl.screen[v.x + 1 + tmp - doom->sdl.size.x] = color;
-		doom->sdl.screen[v.x - 1 + tmp - doom->sdl.size.x] = color;
-		doom->sdl.screen[v.x + tmp + doom->sdl.size.x] = color;
-		doom->sdl.screen[v.x + tmp - doom->sdl.size.x] = color;
+		doom->sdl.screen[pos.x + tmp] = color;
+		doom->sdl.screen[pos.x + 1 + tmp] = color;
+		doom->sdl.screen[pos.x - 1 + tmp] = color;
+		doom->sdl.screen[pos.x + 1 + tmp + doom->sdl.size.x] = color;
+		doom->sdl.screen[pos.x - 1 + tmp + doom->sdl.size.x] = color;
+		doom->sdl.screen[pos.x + 1 + tmp - doom->sdl.size.x] = color;
+		doom->sdl.screen[pos.x - 1 + tmp - doom->sdl.size.x] = color;
+		doom->sdl.screen[pos.x + tmp + doom->sdl.size.x] = color;
+		doom->sdl.screen[pos.x + tmp - doom->sdl.size.x] = color;
 	}
-}
-
-void			trait_id2(t_doom *doom, t_vct2 vct1, t_vct2 vct2, t_minimap mini)
-{
-	int			dx;
-	int			dy;
-	int			d;
-	int			yi;
-
-	dx = vct2.x - vct1.x;
-	dy = vct2.y - vct1.y;
-	yi = 1;
-	if (dy < 0)
-	{
-		yi = -1;
-		dy = -dy;
-	}
-	d = (dy << 1) + dx;
-	while (vct1.x != vct2.x)
-	{
-		vct1.x++;
-		bold_point2(vct1, CWALL, doom, mini);
-		if (d > 0)
-		{
-			vct1.y += yi;
-			d -= dx << 1;
-		}
-		d += dy << 1;
-	}
-}
-
-void			trait_iu2(t_doom *doom, t_vct2 vct1, t_vct2 vct2, t_minimap mini)
-{
-	int			dx;
-	int			dy;
-	int			d;
-	int			xi;
-
-	dy = vct2.y - vct1.y;
-	dx = vct2.x - vct1.x;
-	xi = 1;
-	if (dx < 0)
-	{
-		xi = -1;
-		dx = -dx;
-	}
-	d = (dx << 1) + dy;
-	while (vct1.y != vct2.y)
-	{
-		vct1.y++;
-		bold_point2(vct1, CWALL, doom, mini);
-		if (d > 0)
-		{
-			vct1.x += xi;
-			d -= dy << 1;
-		}
-		d += dx << 1;
-	}
-}
-
-void			trait2(t_doom *doom, t_vct2 vct1, t_vct2 vct2, t_minimap mini)
-{
-	if (vct2.x != -1 && vct2.y != -1
-		&& vct1.x != -1 && vct1.y != -1)
-	{
-		if (abs(vct2.y - vct1.y) < abs(vct2.x - vct1.x))
-		{
-			if (vct1.x > vct2.x)
-				trait_id2(doom, vct2, vct1, mini);
-			else
-				trait_id2(doom, vct1, vct2, mini);
-		}
-		else
-		{
-			if (vct1.y > vct2.y)
-				trait_iu2(doom, vct2, vct1, mini);
-			else
-				trait_iu2(doom, vct1, vct2, mini);
-		}
-	}
+	return (1);
 }
 
 /*
@@ -159,17 +109,63 @@ Uint32			hcol(int health)
 t_vct2		minipoint(t_doom *d, t_fvct2 vct, t_minimap mini)
 {
 	t_vct2	px;
-	//double	rap;
 
-	px.x = (mini.a.x - (mini.size.x >> 1)) + ((vct.x - d->player.pos.x)) * (UNIT);// * (10.0);
-	px.y = (mini.a.y - (mini.size.y >> 1)) + ((vct.y - d->player.pos.y)) * (UNIT);// * (10.0);
-	//px.x = ((vct.x + 10.0) / 20.0) * mini.size.x;
-	//rap = 20.0 / mini.size.x * mini.size.y;
-	//px.y = (((vct.y + rap/2.0) / rap) * mini.size.y);
+	px.x = (mini.a.x - (mini.size.x >> 1)) + ((vct.x - d->player.pos.x)) * (UNIT);
+	px.y = (mini.a.y - (mini.size.y >> 1)) + ((vct.y - d->player.pos.y)) * (UNIT);
 	return (px);
 }
 
-void        miniwalls(t_doom *doom, t_minimap mini, uint32_t color)
+void	minibigline(t_doom *doom, t_vct2 pos0, t_vct2 pos1, t_minimap mini)
+{
+	t_vct3	decal;
+	t_vct2	orig;
+	int		err;
+	int		e2;
+
+	orig.x = ft_abs(pos1.x - pos0.x);
+	orig.y = ft_abs(pos1.y - pos0.y);
+	decal.x = (pos0.x < pos1.x ? 1 : -1);
+	decal.y = (pos0.y < pos1.y ? 1 : -1);
+	err = (orig.x > orig.y ? orig.x : -orig.y) >> 1;
+	bold_point2(doom, mini, pos0, CWALL);
+	while ((pos0.x != pos1.x || pos0.y != pos1.y)
+			&& bold_point2(doom, mini, pos0, CWALL))
+	{
+		e2 = err;
+		if (e2 > -orig.x && ((err -= orig.y) || 1))
+			pos0.x += decal.x;
+		if (e2 < orig.y && ((err += orig.x) || 1))
+			pos0.y += decal.y;
+	}
+}
+
+void	miniline(t_sdl *sdl, t_vct2 pos0, t_vct2 pos1, Uint32 color)
+{
+	t_vct3	decal;
+	t_vct2	orig;
+	int		err;
+	int		e2;
+
+	orig.x = ft_abs(pos1.x - pos0.x);
+	orig.y = ft_abs(pos1.y - pos0.y);
+	decal.x = (pos0.x < pos1.x ? 1 : -1);
+	decal.y = (pos0.y < pos1.y ? 1 : -1);
+	err = (orig.x > orig.y ? orig.x : -orig.y) >> 1;
+	fill_pixel(sdl->screen, sdl->size, pos0, color);
+	while ((pos0.x != pos1.x || pos0.y != pos1.y)
+			&& sdl->screen[pos0.x + pos0.y * sdl->size.x] != WHITE
+			&& sdl->screen[pos0.x + pos0.y * sdl->size.x] != CWALL
+			&& fill_pixel(sdl->screen, sdl->size, pos0, color))
+	{
+		e2 = err;
+		if (e2 > -orig.x && ((err -= orig.y) || 1))
+			pos0.x += decal.x;
+		if (e2 < orig.y && ((err += orig.x) || 1))
+			pos0.y += decal.y;
+	}
+}
+
+void        miniwalls(t_doom *doom, t_minimap mini)
 {
 	int i;
 	t_vct2		cursor;
@@ -177,28 +173,17 @@ void        miniwalls(t_doom *doom, t_minimap mini, uint32_t color)
 	t_vct2		cursor2;
 	t_wall		*wall;
 
-	i = 0;
+	i = -1;
 	wall = doom->sector->wall;
 	cursor = minipoint(doom, wall[0].pillar.p, mini);
 	tmp = cursor;
-	while (i < doom->sector->len - 1)
+	while (++i < doom->sector->len - 1)
 	{
 		cursor2 = minipoint(doom, wall[i + 1].pillar.p, mini);
-		trait2(doom, cursor, cursor2, mini);
-		i++;
+		minibigline(doom, cursor, cursor2, mini);
 		cursor = cursor2;
 	}
-	trait2(doom, cursor, tmp, mini);
-}
-
-
-void		minicursor(t_doom *d, int c, t_minimap mini)
-{
-	t_vct2	cursor;
-
-	cursor.x = mini.a.x - (mini.size.x >> 1);
-	cursor.y = mini.a.y - (mini.size.y >> 1);
-	bold_point2(cursor, c, d, mini);
+	minibigline(doom, cursor, tmp, mini);
 }
 
 void		minifield(t_doom *d, t_minimap mini, double angle)
@@ -206,9 +191,9 @@ void		minifield(t_doom *d, t_minimap mini, double angle)
 
 	t_vct2	pix;
 
-	pix.x = 128 * cos(angle * PI180) + mini.mid.x;
-	pix.y = -128 * sin(angle * PI180) + mini.mid.y;
-	fill_line(&d->sdl, mini.mid, pix, hcol(d->player.health));
+	pix.x = 256 * cos(angle * PI180) + mini.mid.x;
+	pix.y = -256 * sin(angle * PI180) + mini.mid.y;
+	miniline(&d->sdl, mini.mid, pix, hcol(d->player.health));
 }
 
 void		minimap(t_doom *d)
@@ -238,18 +223,15 @@ void		minimap(t_doom *d)
 		d->sdl.screen[i + j * d->sdl.size.x] = WHITE;
 		++i;
 	}
-	miniwalls(d, mini, CWALL);
+	miniwalls(d, mini);
 	i = (d->player.rot.y - (d->player.fov >> 1));
 	while (i < (d->player.rot.y + (d->player.fov >> 1)))
 	{
 		minifield(d, mini, i);
-		i += d->player.fov / 4;
+		i += d->player.fov >> 3;
 	}
-	miniwalls(d, mini, CWALL);
-	//minifield(d, mini, (d->player.rot.y + (d->player.fov >> 1)));
-	//minifield(d, mini, (d->player.rot.y - (d->player.fov >> 1)));
-	//minifield(d, mini, (d->player.rot.y));
-	minicursor(d, CPERS, mini); //mid
+	miniwalls(d, mini);
+	bold_point2(d, mini, mini.mid, CPERS);
 	debug_player(d->player);
 	SDL_RenderCopy(d->sdl.rend, d->sdl.txture, NULL, NULL);
 }
