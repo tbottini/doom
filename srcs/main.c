dc0ea3777dad770fc459f7b1ccb83b9e9c71cede
tbottini <thomasbottini@protonmail.com>
2019-05-20 22:07:33 +0200

    modif
    Former-commit-id: 7fa9287e24ac0739d208cd9405c6907f22b540ea [formerly 1b87d8596c0db234facad0ec452438231d60933e]
    Former-commit-id: 057fcd7a1d32f81912cf436fcd646335abcf2661

--- srcs/main.c
+++ srcs/main.c
@@ -1,20 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/16 00:18:50 by magrab            #+#    #+#             */
-/*   Updated: 2019/05/17 17:21:26 by tbottini         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "doom_nukem.h"
 
 static int	init(void)
 {
-	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_GAMECONTROLLER|SDL_INIT_TIMER) != 0)
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_GAMECONTROLLER) != 0)
 	{
 		ft_printf("SDL_Init: %s\n", SDL_GetError());
 		return (0);
@@ -35,26 +23,27 @@ static int	init(void)
 int			main(int ac, char **av)
 {
 	t_doom		*doom;
+	SDL_Event	event;
 
+	if (ac < 2)
+		return (-1);
 	if (!init())
 		return (0);
 	if (!(doom = doom_init()))
 		return (-1);
-	if (ac == 2)
+	if (!(parsing(doom, av[1])))
 	{
-		if (parsing(doom, av[1]))
-			sdl_set_status(doom, 0);
-		else
-			sdl_set_status(doom, 1);
+		printf("bad parsing\n");
+		return (0);
 	}
-	else
-		sdl_set_status(doom, 1);
+	describe_player(doom->player);
+	describe_sector(*doom->sector);
+	portal_engine(doom);
 	while (1)
 	{
-		if (!(event_handler(doom)))
-			return (0);
-		loop_hook(doom);
+		SDL_WaitEvent(&event);
+		if (event.type == SDL_KEYDOWN)
+			keyboard_input(doom, event);
 	}
 	return (0);
 }
-
