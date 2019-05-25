8262e65b6f27fe02558d6aefecc0ecb5e5d910b7
Armand KRACHE <Armandkrache@gmail.com>
2019-05-25 23:39:21 +0200

    correction Reperer inverse + ajout collisions
    Former-commit-id: 67227dbfd0c28bd2a3fae42747d94736c3648679
    Former-commit-id: f7ed1bf0179b213716071827df79fcd2b7d31b21

--- srcs/input/move.c
+++ srcs/input/move.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   move.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akrache <akrache@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 15:13:17 by akrache           #+#    #+#             */
-/*   Updated: 2019/05/19 17:47:33 by tbottini         ###   ########.fr       */
+/*   Updated: 2019/05/25 23:37:31 by akrache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ void		crouch(t_player *player)
 	if (!(player->crouch))
 	{
 		player->crouch = 1;
-		player->speed >>= 1;
-		player->height >>= 1;
+		player->speed /= 2;
+		player->height /= 2;
 	}
 }
 
 void		crouch_release(t_player *player)
 {
 	player->crouch = 0;
-	player->speed >>= 1;
-	player->height >>= 1;
+	player->speed *= 2;
+	player->height *= 2;
 }
 
 void		sprint(t_player *player)
@@ -41,10 +41,12 @@ void		sprint_release(t_player *player)
 	player->speed = 10;
 }
 
-void		move(t_player *player, int x, int y)
+void		move(t_doom *doom, t_player *player, int x, int y)
 {
-	t_fvct2 d;
-	t_fvct3 npos;
+	t_fvct2	d;
+	t_fvct3	npos;
+	t_fvct3	tmp;
+	int		w;
 
 	// Update Rotation
 	npos.z = player->pos.z;
@@ -57,9 +59,30 @@ void		move(t_player *player, int x, int y)
 	d.x = sin(player->rot.y * PI180) / 10.0;
 	d.y = cos(player->rot.y * PI180) / 10.0;
 	npos.x = player->pos.x + d.x * y / 35000.0 + d.y * 1 * x / 35000.0;
-	npos.y = player->pos.y + d.x * -x / 35000.0 + d.y * y / 35000.0;
+	//npos.y = player->pos.y + d.x * -x / 35000.0 + d.y * y / 35000.0;
+	npos.y = player->pos.y - d.x * -x / 35000.0 - d.y * y / 35000.0;
+	if ((w = collisionV42(doom, npos, -1)) == -1)
+	{
+		player->pos.x = npos.x;
+		player->pos.y = npos.y;
+		return (ft_putendl("OK"));
+	}
+	tmp.x = player->pos.x;
+	tmp.y = npos.y;
+	if (!collisionV42(doom, tmp, w))
+	{
+		player->pos.y = npos.y;
+		return (ft_putendl("Y OK Y"));
+	}
+	tmp.y = player->pos.y;
+	tmp.x = npos.x;
+	if (!collisionV42(doom, tmp, w))
+	{
+		player->pos.x = npos.x;
+		return (ft_putendl("X OK X"));
+	}
 	// Check new pos
 	//DEPRECATED if (0 < npos.x && npos.x < doom->map_size.x && 0 < npos.y && npos.y < doom->map_size.y)
 	//DEPRECATED	if (doom->map[(int)npos.y][(int)npos.x] != '#')
-	player->pos = npos;
+	//player->pos = npos; //ancien aui marche
 }
