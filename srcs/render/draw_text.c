9fe8fdc0dd8397b33f3e4d93eff5da7445bb99d3
Maxence G <maxence.gr@hotmail.fr>
2019-05-25 17:39:43 +0200

    Add Int text to screen function
    Former-commit-id: 72639f97d2ae89de3fc6adee63bdee1a32a1dc91 [formerly 8c162c773e1496aa53be3aa961dba91508492a8c]
    Former-commit-id: 1ad23e4b3cd40b035e5d303ae95520a885e421f5

--- srcs/render/draw_text.c
+++ srcs/render/draw_text.c
@@ -1,6 +1,6 @@
 #include "doom_nukem.h"
 
-int		sdl_string_put(SDL_Renderer *rend, TTF_Font *font, t_vct2 loc, const char *text, SDL_Color fg)
+int sdl_string_put(SDL_Renderer *rend, TTF_Font *font, t_vct2 loc, const char *text, SDL_Color fg)
 {
 	SDL_Texture *texture;
 	SDL_Surface *surf;
@@ -14,5 +14,18 @@ int		sdl_string_put(SDL_Renderer *rend, TTF_Font *font, t_vct2 loc, const char *
 	SDL_RenderCopy(rend, texture, NULL, &pos);
 	SDL_DestroyTexture(texture);
 	SDL_FreeSurface(surf);
+	return (0);
+}
+
+int sdl_int_put(SDL_Renderer *rend, TTF_Font *font, t_vct2 loc, const char *label, const int value, SDL_Color fg)
+{
+	char *str;
+	char *val;
+
+	val = ft_itoa(value);
+	str = ft_strjoin(label, val);
+	sdl_string_put(rend, font, loc, str, fg);
+	free(val);
+	free(str);
 	return (0);
 }
