1558d98f5a0d7c45294ce6267049740aa58cc085
tbottini <thomasbottini@protonmail.com>
2019-05-27 18:21:06 +0200

    backface angle 0

--- srcs/render/backface.c
+++ srcs/render/backface.c
@@ -6,24 +6,17 @@
 **	on la compare a celle actuelle via l'index
 **		si elle est plus proche on renvoie 1 pour qu'on puisse
 **		calculer et afficher la colomne
+**	tout est inverse car on ne recoit pas la distance mais la taille du pillier
 */
-int			z_line_buffer(t_doom doom, double dist, int px)
+int			z_line_buffer(t_doom doom, double len_pillar, int px)
 {
 	static int ap = 0;
 	double	deep;
 	double	angle;
 
-	ap++;
-	//determiner l'angle du pillier par rapport au point de vue du
-	//joueur avec l'id du pixel
 	angle = ((double)px / doom.sdl.size.x) * doom.player.fov - (doom.player.fov / 2.0);
-	deep = sin(-fabs(angle) * PI180) * dist;
-	if (ap % 100 == 0)
-	{
-		//printf("px %d angle %f\n", px , angle);
-		printf("angle %f dist %f deep %f\n", angle, dist, deep);
-	}
-	if (deep < doom.zline[px])
+	deep = cos(fabs(angle) * PI180) * len_pillar;
+	if (deep > doom.zline[px])
 	{
 		doom.zline[px] = deep;
 		return (1);
