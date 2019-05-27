449405c36070590a96972980bd7a889523c39f6e
tbottini <thomasbottini@protonmail.com>
2019-05-27 15:32:56 +0200

    fix bug render for reverse map
    Former-commit-id: b83534ee824eaca18542398e28abf5609b56d23e
    Former-commit-id: 511d13d5700f4e1fca6afba0b4b3f1639efc2419

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -1,9 +1,24 @@
 #include "doom_nukem.h"
 
+void				sector_render(t_doom *doom, t_sector *sector)
+{
+	t_wall			*bunch[50];
+
+	sector_frustum(sector, doom->player);
+	buncherisation(*sector, bunch);
+	bunch_comsuption(doom, bunch);
+}
+
 
 int					doom_render(t_doom *doom)
 {
-	portal_engine(doom);
+	//portal_engine(doom);
+
+	//on rend un modele
+
+	sector_render(doom, doom->sector);
+	if (doom->sector->len_sub)
+		sector_render(doom, &doom->sector->ssector[0]);
 	minimap(doom);
 	sdl_present(&doom->sdl);
 	return (1);
