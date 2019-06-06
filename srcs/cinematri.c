f31c1d90d3b46d3c381db4ff55175e8eb6024a4c
Armand KRACHE <Armandkrache@gmail.com>
2019-06-06 23:32:37 +0200

    Ajout ressources Cinematic et musiques
    Former-commit-id: 970690fcbebb973cadf242c5ca93dd33e170b86e [formerly c856f6f48fe9e00d98625b90eb1227e5339cc635]
    Former-commit-id: 6e6f296e17bab41731f94501914017f0ad1c23ed
    Former-commit-id: 3e2f94464f02069ebb081191af9ba4f2a53d9ae7

--- srcs/cinematri.c
+++ srcs/cinematri.c
@@ -6,7 +6,7 @@
 /*   By: akrache <akrache@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 01:54:40 by akrache           #+#    #+#             */
-/*   Updated: 2019/06/05 22:34:28 by akrache          ###   ########.fr       */
+/*   Updated: 2019/06/06 23:30:18 by akrache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,22 +58,22 @@ static void	cine_delay(t_doom *doom, int *i)
 
 static void	concat_atoi(char *deb, int i)
 {
-	deb[39] = i / 1000 + '0';
+	deb[27] = i / 1000 + '0';
 	i %= 1000;
-	deb[40] = i / 100 + '0';
+	deb[28] = i / 100 + '0';
 	i %= 100;
-	deb[41] = i / 10 + '0';
+	deb[29] = i / 10 + '0';
 	i %= 10;
-	deb[42] = i + '0';
+	deb[30] = i + '0';
 }
 
 static void	super_cinematrique(t_doom *doom, SDL_Surface *image, SDL_Texture *texture, int i)
 {
-	char		deb[48];
+	char		deb[36];
 	int			max;
 
 	max = i == 200 ? 3000 : 138;
-	ft_strcpy(deb, "/Users/akrache/Desktop/cinematic/frame_0000.png");//change path
+	ft_strcpy(deb, "ressources/cinematic/frame_0000.png");//change path
 	while (i <= max && (image = IMG_Load(deb)))
 	{
 		concat_atoi(deb, i);
@@ -100,8 +100,8 @@ void	cinematrique(t_doom *doom)
 	SDL_Texture	*texture;
 
 	i = 0;
-	ft_strcpy(deb, "/Users/akrache/Desktop/cinematic/frame_0000.png");//change path
-	if (!(image = IMG_Load("/Users/akrache/Desktop/cinematic/frame_0000.png")))//change path
+	ft_strcpy(deb, "ressources/cinematic/frame_0000.png");//change path
+	if (!(image = IMG_Load("ressources/cinematic/frame_0000.png")))//change path
 		return ;
 	if (!(texture = SDL_CreateTextureFromSurface(doom->sdl.rend, image)))
 		return ;
