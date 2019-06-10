76529d0184a0d630c09f7ccfb0276891ed1a9d4d
tbottini <thomasbottini@protonmail.com>
2019-06-10 10:20:08 +0200

    Rectif Gameplay bug

--- srcs/manager/player_manager.c
+++ srcs/manager/player_manager.c
@@ -6,7 +6,7 @@
 /*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 22:08:13 by magrab            #+#    #+#             */
-/*   Updated: 2019/06/07 13:14:40 by tbottini         ###   ########.fr       */
+/*   Updated: 2019/06/10 10:19:24 by tbottini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ int		player_init(t_player *player)
 	player->fov = 90;
 	player->health = 100;
 	player->weight = 10;
+	player->crouch = 0;
 	return (1);
 }
