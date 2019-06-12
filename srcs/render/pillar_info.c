cadd54eee28b26725d2dd8c7e25a13d5f2da53fb
tbottini <thomasbottini@protonmail.com>
2019-06-12 16:50:04 +0200

    change structure
    Former-commit-id: 240ca1084995c3d0c7a67aa121a5c622bb31d313 [formerly 8e82f4b2d11e872a7ad2db17e60016bc31f73b29]
    Former-commit-id: 44ba809c130788f8d1b418e3131565e905328778
    Former-commit-id: a79b8ad84d0768bdb83ee6dfc8063aec95e9c39b

--- srcs/render/pillar_info.c
+++ srcs/render/pillar_info.c
@@ -24,65 +24,61 @@ double		pillar_polarite(t_pillar pillar, t_pillar next, int max)
 	return ((polarite == -1) ? 0 : max);
 }
 
-void			pillar_screen_info(t_doom doom, t_wall wall, t_fvct2 *dist, t_vct2 *column_id)
+void			pillar_screen_info(t_doom *doom, t_wall wall, t_fvct2 *dist, t_vct2 *column_id)
 {
-	t_vct2 		px;
-	t_fvct2 	d;
 	float		angle;
 	t_player	*p;
 	int			size;
 
-	p = &doom.player;
-	size = doom.sdl.size.x;
+	p = &doom->player;
+	size = doom->sdl.size.x;
 	if (wall.pillar.frust)
 	{
-		px.x = fish_bowl_px(doom, wall.pillar);
-		d.x = distance(*(t_fvct2*)&p->pos, wall.pillar.p);
-		fish_eyes(&d.x, wall.pillar.angle);
+		doom->tool.px.x = fish_bowl_px(doom, wall.pillar);
+		doom->tool.dist.x = distance(*(t_fvct2*)&p->pos, wall.pillar.p);
+		fish_eyes(&doom->tool.dist.x, wall.pillar.angle);
 	}
 	else
 	{
-		px.x = pillar_polarite(*wall.next, wall.pillar, size - 1);
-		angle = (px.x == 0) ? p->rot.y + p->fov / 2.0 : p->rot.y - p->fov / 2.0;
-		d.x = wall_clipping(wall, *(t_fvct2*)&p->pos, angle);
-		fish_eyes(&d.x, angle - p->rot.y);
+		doom->tool.px.x = pillar_polarite(*wall.next, wall.pillar, size - 1);
+		angle = (doom->tool.px.x == 0) ? p->rot.y + p->fov / 2.0 : p->rot.y - p->fov / 2.0;
+		doom->tool.dist.x = wall_clipping(wall, *(t_fvct2*)&p->pos, angle);
+		fish_eyes(&doom->tool.dist.x, angle - p->rot.y);
 	}
 	if (wall.next->frust)
 	{
-		px.y = fish_bowl_px(doom, *wall.next);
-		d.y = distance(*(t_fvct2*)&p->pos, wall.next->p);
-		fish_eyes(&d.y, wall.next->angle);
+		doom->tool.px.y = fish_bowl_px(doom, *wall.next);
+		doom->tool.dist.y = distance(*(t_fvct2*)&p->pos, wall.next->p);
+		fish_eyes(&doom->tool.dist.y, wall.next->angle);
 	}
 	else
 	{
-		px.y = pillar_polarite(wall.pillar, *wall.next, size - 1);
-		angle = (px.y == 0) ? p->rot.y + p->fov / 2.0 : p->rot.y - p->fov / 2.0;
-		d.y = wall_clipping(wall, *(t_fvct2*)&p->pos, angle);
-		fish_eyes(&d.y, angle - p->rot.y);
+		doom->tool.px.y = pillar_polarite(wall.pillar, *wall.next, size - 1);
+		angle = (doom->tool.px.y == 0) ? p->rot.y + p->fov / 2.0 : p->rot.y - p->fov / 2.0;
+		doom->tool.dist.y = wall_clipping(wall, *(t_fvct2*)&p->pos, angle);
+		fish_eyes(&doom->tool.dist.y, angle - p->rot.y);
 	}
-	*column_id = px;
-	*dist = d;
 }
 
 /*
 **	on calcul la portion de l'ecran appartenant au mur
 **	sector : recup la hauteur au plafond,
 */
-t_vct2			sector_starend(t_sector sector, double dist, t_doom doom)
+t_vct2			sector_starend(t_sector sector, double dist, t_doom *doom)
 {
 
 	t_vct2		surface;
 	double		point;
 	double		alpha;
 
-	point = sector.h_ceil - doom.player.height;
+	point = sector.h_ceil - doom->player.height;
 	//dist_point = sqrt(dist * dist + up * up);
-	alpha = (doom.player.rot.x - 90) * PI180 - atan(dist / point);
-	surface.x = doom.sdl.size.y / 2.0 - tan(alpha) * doom.camera.d_screen;
+	alpha = (doom->player.rot.x - 90) * PI180 - atan(dist / point);
+	surface.x = doom->sdl.size.y / 2.0 - tan(alpha) * doom->camera.d_screen;
 
-	point = -doom.player.height;
-	alpha = (doom.player.rot.x - 90) * PI180 - atan(dist / point);
-	surface.y = doom.sdl.size.y / 2.0 - tan(alpha) * doom.camera.d_screen;
+	point = -doom->player.height;
+	alpha = (doom->player.rot.x - 90) * PI180 - atan(dist / point);
+	surface.y = doom->sdl.size.y / 2.0 - tan(alpha) * doom->camera.d_screen;
 	//on recupere la distance x du point celon le point de vue
 	return (surface);
 }
