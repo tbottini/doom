4860fa5393fe3f022e4daf516f38fe7b0e4f0d7d
Maxence G <maxence.gr@hotmail.fr>
2019-06-12 16:33:17 +0200

    Moar multithread
    Former-commit-id: 421435f9267b2562b8570351d66e3d56b19c5b52 [formerly d358c5dd7eab4866c0727d14eb63ffea873dc01f]
    Former-commit-id: 09a75ee2d35812d5b35a42fae7916da41ef94bb9
    Former-commit-id: 6bb103b9f8d5750cf47f243adf08d4edfc3d5252

--- srcs/tools/sdl_tools.c
+++ srcs/tools/sdl_tools.c
@@ -1,6 +1,6 @@
 #include "doom_nukem.h"
 
-static int upper_left(void *ptr)
+static int upper_left_g(void *ptr)
 {
 	SDL_Rect part;
 	t_sdl *sdl;
@@ -8,19 +8,19 @@ static int upper_left(void *ptr)
 	sdl = ptr;
 	part.x = 0;
 	part.y = 0;
-	part.w = sdl->size.x / 2;
+	part.w = sdl->size.x / 4;
 	part.h = sdl->size.y / 2;
 	SDL_RenderCopy(sdl->rend, sdl->txture, &part, &part);
 	return (0);
 }
 
-static int upper_right(void *ptr)
+static int upper_left_d(void *ptr)
 {
 	SDL_Rect part;
 	t_sdl *sdl;
 
 	sdl = ptr;
-	part.x = sdl->size.x / 2;
+	part.x = sdl->size.x / 4;
 	part.y = 0;
 	part.w = part.x;
 	part.h = sdl->size.y / 2;
@@ -28,7 +28,35 @@ static int upper_right(void *ptr)
 	return (0);
 }
 
-static int bottom_left(void *ptr)
+static int upper_right_g(void *ptr)
+{
+	SDL_Rect part;
+	t_sdl *sdl;
+
+	sdl = ptr;
+	part.x = sdl->size.x / 2;
+	part.y = 0;
+	part.w = part.x / 2;
+	part.h = sdl->size.y / 2;
+	SDL_RenderCopy(sdl->rend, sdl->txture, &part, &part);
+	return (0);
+}
+
+static int upper_right_d(void *ptr)
+{
+	SDL_Rect part;
+	t_sdl *sdl;
+
+	sdl = ptr;
+	part.w = sdl->size.x / 4;
+	part.h = sdl->size.y / 2;
+	part.x = sdl->size.x - part.w;
+	part.y = 0;
+	SDL_RenderCopy(sdl->rend, sdl->txture, &part, &part);
+	return (0);
+}
+
+static int bottom_left_g(void *ptr)
 {
 	SDL_Rect part;
 	t_sdl *sdl;
@@ -36,13 +64,27 @@ static int bottom_left(void *ptr)
 	sdl = ptr;
 	part.x = 0;
 	part.y = sdl->size.y / 2;
-	part.w = sdl->size.x / 2;
+	part.w = sdl->size.x / 4;
 	part.h = part.y;
 	SDL_RenderCopy(sdl->rend, sdl->txture, &part, &part);
 	return (0);
 }
 
-static int bottom_right(void *ptr)
+static int bottom_left_d(void *ptr)
+{
+	SDL_Rect part;
+	t_sdl *sdl;
+
+	sdl = ptr;
+	part.x = sdl->size.x / 4;
+	part.y = sdl->size.y / 2;
+	part.w = part.x;
+	part.h = part.y;
+	SDL_RenderCopy(sdl->rend, sdl->txture, &part, &part);
+	return (0);
+}
+
+static int bottom_right_g(void *ptr)
 {
 	SDL_Rect part;
 	t_sdl *sdl;
@@ -50,24 +92,46 @@ static int bottom_right(void *ptr)
 	sdl = ptr;
 	part.x = sdl->size.x / 2;
 	part.y = sdl->size.y / 2;
-	part.w = part.x;
+	part.w = part.x / 2;
 	part.h = part.y;
+	SDL_RenderCopy(sdl->rend, sdl->txture, &part, &part);
+	return (0);
+}
+
+static int bottom_right_d(void *ptr)
+{
+	SDL_Rect part;
+	t_sdl *sdl;
+
+	sdl = ptr;
+	part.w = sdl->size.x / 4;
+	part.h = sdl->size.y / 2;
+	part.x = sdl->size.x - part.w;
+	part.y = part.h;
 	SDL_RenderCopy(sdl->rend, sdl->txture, &part, &part);
 	return (0);
 }
 
 void	sdl_MultiRenderCopy(t_sdl *sdl)
 {
-	SDL_Thread *rend_ths[4];
+	SDL_Thread *rend_ths[8];
 
-	rend_ths[0] = SDL_CreateThread(&upper_left, "Upper Left screen", sdl);
-	rend_ths[1] = SDL_CreateThread(&upper_right, "Upper Right screen", sdl);
-	rend_ths[2] = SDL_CreateThread(&bottom_left, "Bottom Left screen", sdl);
-	rend_ths[3] = SDL_CreateThread(&bottom_right, "Bottom Right screen", sdl);
+	rend_ths[0] = SDL_CreateThread(&upper_left_g, "Upper Left screen", sdl);
+	rend_ths[1] = SDL_CreateThread(&upper_left_d, "Upper Left screen", sdl);
+	rend_ths[2] = SDL_CreateThread(&upper_right_g, "Upper Right screen", sdl);
+	rend_ths[3] = SDL_CreateThread(&upper_right_d, "Upper Right screen", sdl);
+	rend_ths[4] = SDL_CreateThread(&bottom_left_g, "Bottom Left screen", sdl);
+	rend_ths[5] = SDL_CreateThread(&bottom_left_d, "Bottom Left screen", sdl);
+	rend_ths[6] = SDL_CreateThread(&bottom_right_g, "Bottom Right screen", sdl);
+	rend_ths[7] = SDL_CreateThread(&bottom_right_d, "Bottom Right screen", sdl);
 	SDL_WaitThread(rend_ths[0], NULL);
 	SDL_WaitThread(rend_ths[1], NULL);
 	SDL_WaitThread(rend_ths[2], NULL);
 	SDL_WaitThread(rend_ths[3], NULL);
+	SDL_WaitThread(rend_ths[4], NULL);
+	SDL_WaitThread(rend_ths[5], NULL);
+	SDL_WaitThread(rend_ths[6], NULL);
+	SDL_WaitThread(rend_ths[7], NULL);
 }
 
 
