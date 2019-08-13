ecf4ab4dfd5c75450678f55c521a6d9e6c63fde1
tbottini <thomasbottini@protonmail.com>
2019-08-13 07:02:43 +0200

    je cague le cul aux modif de max

--- srcs/render/pre_render/wall.c
+++ srcs/render/pre_render/wall.c
@@ -6,7 +6,7 @@
 /*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 21:55:18 by tbottini          #+#    #+#             */
-/*   Updated: 2019/08/13 04:24:57 by tbottini         ###   ########.fr       */
+/*   Updated: 2019/08/13 06:59:48 by tbottini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,101 +18,101 @@
 **	renvoie le pourcentage de l'intersection par rapport au mur (debut pilier)
 */
 
-double		wall_clipping(t_arch *arch, t_player *p,
-		t_fvct2 *inter_local, double angle)
+double			wall_clipping(t_arch *arch, t_player *p, t_fvct2 *inter_local, double angle)
 {
-	t_fvct3		inter;
-	t_fvct2		d;
-	t_fvct2		d2;
-	t_fvct3		t;
+	t_fvct2		inter;
+	t_fvct2		diff;
+	t_fvct2		diff2;
+	double		coef_ang;
+	double		coef_wall;
+	double		b;
+	double		percent;
 
-	d = (t_fvct2){arch->wall->pillar->p.x - p->stat.pos.x,
-		arch->wall->pillar->p.y - p->stat.pos.y};
-	d2 = (t_fvct2){arch->wall->next->p.x - p->stat.pos.x,
-		arch->wall->next->p.y - p->stat.pos.y};
-	t.x = tan(angle * PI180);
-	if (d2.x == d.x && d2.x == d.x)
-		inter = (t_fvct3){d.x, d.x * t.x, (d2.y - inter.y) / (d2.y - d.y)};
+	diff.x = arch->wall->pillar->p.x - p->stat.pos.x;
+	diff.y = arch->wall->pillar->p.y - p->stat.pos.y;
+	diff2.x = arch->wall->next->p.x - p->stat.pos.x;
+	diff2.y = arch->wall->next->p.y - p->stat.pos.y;
+	coef_ang = tan(angle * PI180);
+	if (diff2.x == diff.x && diff2.x == diff.x)
+	{
+		inter.x = diff.x;
+		inter.y = diff.x * coef_ang;
+		percent = (diff2.y - inter.y) / (diff2.y - diff.y);
+	}
 	else
 	{
-		t.y = (d2.y - d.y) / (d2.x - d.x);
-		t.z = d.y - d.x * t.y;
-		inter.x = t.z / (t.x - t.y);
-		inter.y = t.y * inter.x + t.z;
-		inter.z = (d2.x - inter.x) / (d2.x - d.x);
+		coef_wall = (diff2.y - diff.y) / (diff2.x - diff.x);
+		b = diff.y - diff.x * coef_wall;
+		inter.x = b / (coef_ang - coef_wall);
+		inter.y = coef_wall * inter.x + b;
+		percent = (diff2.x - inter.x) / (diff2.x - diff.x);
 	}
-	inter_local->y = hypothenuse((t_fvct2){inter.x, inter.y});
+	inter_local->y = hypothenuse(inter);
 	inter_local->x = cos((angle - p->stat.rot.y) * PI180) * inter_local->y;
 	inter_local->y = sin((angle - p->stat.rot.y) * PI180) * inter_local->y;
-	return (inter.z);
+	return (percent);
 }
 
 /*
 **	va separer la porte en deux partie un portail et un mur
 */
 
-static void	if_door_visible(t_arch *arch, t_player *player,
-		t_fvct2 i, double po)
+void				door_split(t_arch *arch, t_player *player, int flag)
 {
+	double			percent_open;
+	double			percent_local;
+	t_fvct2			inter;
 	int				px_tmp;
 	double			shift_txtr_tmp;
 	t_fvct2			next_tmp;
 
-	px_tmp = arch->px.y;
-	shift_txtr_tmp = arch->shift_txtr.y;
-	next_tmp = arch->next;
-	arch->px.y = arch->sdl->size.x / 2 - arch->sdl->size.x / 2 * (i.y / i.x);
-	arch->next = i;
-	arch->shift_txtr.x = 1 - po + (1 - arch->shift_txtr.x);
-	if (arch->shift_txtr.y > 1 - po)
-		arch->shift_txtr.y = 1 - (arch->shift_txtr.y - (1 - po));
-	else
-		arch->shift_txtr.y = 1;
-	arch->wall->status = WALL;
-	reorder(arch);
-	render_surface(arch, player);
-	arch->wall->status = PORTAL;
-	arch->px.x = arch->px.y;
-	arch->px.y = px_tmp;
-	arch->pillar = arch->next;
-	arch->next = next_tmp;
-	arch->shift_txtr = (t_fvct2){arch->shift_txtr.y, shift_txtr_tmp};
-	reorder(arch);
-	render_surface(arch, player);
-}
-
-void		lil_door_split(t_arch *arch, t_player *player)
-{
-	arch->wall->status = PORTAL;
-	reorder(arch);
-	render_surface(arch, player);
-}
-
-void		door_split(t_arch *arch, t_player *player, int flag)
-{
-	double			po;
-	double			pl;
-	t_fvct2			inter;
-
-	po = (arch->timestamp - arch->wall->ots) / ((double)DOOR_OPEN_TIME);
+	percent_open = (arch->timestamp - arch->wall->ots) / ((double)DOOR_OPEN_TIME);
 	if (flag == OPEN_DOOR)
-		po = 1 - po;
-	if (po > 1)
-		po = 1;
-	if (po < 0)
-		po = 0;
-	pl = (arch->shift_txtr.x - (1 - po))
-		/ (arch->shift_txtr.x - arch->shift_txtr.y);
-	if (pl > 1)
+		percent_open = 1 - percent_open;
+	if (percent_open > 1)
+		percent_open = 1;
+	if (percent_open < 0)
+		percent_open = 0;
+	percent_local = (arch->shift_txtr.x - (1 - percent_open)) / (arch->shift_txtr.x - arch->shift_txtr.y);
+	if (percent_local > 1)
 		inter = arch->next;
 	else
-		inter = (t_fvct2){arch->pillar.x + pl * (arch->next.x - arch->pillar.x),
-			arch->pillar.y + pl * (arch->next.y - arch->pillar.y)};
+	{
+		inter.x = arch->pillar.x + percent_local * (arch->next.x - arch->pillar.x);
+		inter.y = arch->pillar.y + percent_local * (arch->next.y - arch->pillar.y);
+	}
 	if (arch->shift_txtr.y > 1)
 		return ;
-	if (1 - arch->shift_txtr.x < po)
-		if_door_visible(arch, player, inter, po);
+	if (1 - arch->shift_txtr.x < percent_open)
+	{
+		px_tmp = arch->px.y;
+		shift_txtr_tmp = arch->shift_txtr.y;
+		next_tmp = arch->next;
+		arch->px.y = arch->sdl->size.x / 2 - arch->sdl->size.x / 2 * (inter.y / inter.x);
+		arch->next = inter;
+		arch->shift_txtr.x = 1 - percent_open + (1 - arch->shift_txtr.x);
+		if (arch->shift_txtr.y > 1 - percent_open)
+			arch->shift_txtr.y = 1 - (arch->shift_txtr.y - (1 - percent_open));
+		else
+			arch->shift_txtr.y = 1;
+		arch->wall->status = WALL;
+		reorder(arch);
+		render_surface(arch, player);
+		arch->wall->status = PORTAL;
+		arch->px.x = arch->px.y;
+		arch->px.y = px_tmp;
+		arch->pillar = arch->next;
+		arch->next = next_tmp;
+		arch->shift_txtr.x = arch->shift_txtr.y;
+		arch->shift_txtr.y = shift_txtr_tmp;
+		reorder(arch);
+		render_surface(arch, player);
+	}
 	else
-		lil_door_split(arch, player);
+	{
+		arch->wall->status = PORTAL;
+		reorder(arch);
+		render_surface(arch, player);
+	}
 	arch->wall->status = flag;
 }
