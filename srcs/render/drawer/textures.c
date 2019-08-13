41639fe410582ed6c095a4f1d7675a28a8b9adc5
tbottini <thomasbottini@protonmail.com>
2019-08-13 04:36:25 +0200

    t\'inquietes pas ca va bien se passer

--- srcs/render/drawer/textures.c
+++ srcs/render/drawer/textures.c
@@ -6,39 +6,37 @@
 /*   By: tbottini <tbottini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 10:53:43 by tbottini          #+#    #+#             */
-/*   Updated: 2019/08/13 03:00:44 by tbottini         ###   ########.fr       */
+/*   Updated: 2019/08/13 04:35:57 by tbottini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 
-double			percent_interpolation2d(t_arch *arch)
+double				percent_interpolation2d(t_arch *a)
 {
-	t_affine	px_affine;
-	t_affine	wall_affine;
-	t_fvct2		inter;
-	double		percent;
+	t_affine		px_affine;
+	t_affine		w;
+	t_fvct2			inter;
+	double			percent;
 
-	px_affine.a = ((arch->sdl->size.x / 2) - arch->px.x) / arch->cam->d_screen;
+	px_affine.a = ((a->sdl->size.x / 2) - a->px.x) / a->cam->d_screen;
 	px_affine.b = 0;
-	if (arch->pillar.y == arch->next.y)
+	if (a->pillar.y == a->next.y)
 	{
-		inter.x = arch->next.x;
+		inter.x = a->next.x;
 		inter.y = px_affine.a * inter.x;
-		percent = (inter.y - arch->pillar.y) / (arch->next.y - arch->pillar.y);
+		percent = (inter.y - a->pillar.y) / (a->next.y - a->pillar.y);
 	}
 	else
 	{
-		wall_affine.a = (arch->next.y - arch->pillar.y) / (arch->next.x - arch->pillar.x);
-		wall_affine.b = arch->pillar.y - wall_affine.a * arch->pillar.x;
-		inter = interpolation_linear(wall_affine, px_affine);
-		percent = (inter.x - arch->pillar.x) / (arch->next.x - arch->pillar.x);
+		w.a = (a->next.y - a->pillar.y) / (a->next.x - a->pillar.x);
+		w.b = a->pillar.y - w.a * a->pillar.x;
+		inter = interpolation_linear(w, px_affine);
+		percent = (inter.x - a->pillar.x) / (a->next.x - a->pillar.x);
 	}
-	percent = percent * (arch->shift_txtr.y -  arch->shift_txtr.x) + arch->shift_txtr.x;
-	if (percent < 0)
-		return (0);
-	else if (percent > 1)
-		return (1);
+	percent = percent * (a->shift_txtr.y - a->shift_txtr.x) + a->shift_txtr.x;
+	if (percent < 0 || percent > 1)
+		percent = (percent < 0) ? 0 : 1;
 	return (percent);
 }
 
@@ -48,20 +46,23 @@ double			percent_interpolation2d(t_arch *arch)
 **	pblm on peut avoir un mur face au joueur formant
 **	une affine constante verticalement
 */
-uint32_t		texture_interpolation2d(t_arch *arch, t_txtr *txtr)
+
+uint32_t			texture_interpolation2d(t_arch *arch, t_txtr *txtr)
 {
-	double		percent;
+	double			percent;
 
 	percent = percent_interpolation2d(arch);
 	return (percent * txtr->w);
 }
 
-uint32_t		texture_prop_interpolation2d(t_arch *arch, t_txtr *txtr, t_prop *prop)
+uint32_t			texture_prop_interpolation2d(t_arch *arch, t_txtr *txtr
+	, t_prop *prop)
 {
-	double		percent;
+	double			percent;
 
 	percent = percent_interpolation2d(arch);
-	percent = 1 - (percent - prop->percent.x) / (prop->percent.y - prop->percent.x);
+	percent = 1 - (percent - prop->percent.x) / (prop->percent.y
+		- prop->percent.x);
 	if (percent < 0)
 		return (0);
 	return (percent * txtr->w);
@@ -72,11 +73,12 @@ uint32_t		texture_prop_interpolation2d(t_arch *arch, t_txtr *txtr, t_prop *prop)
 **	les deux distance et le nouveau point d'intersection
 **	-flag definit quel pillier doit changer
 */
-void			pillar_virtual_move(t_arch *arch, t_fvct2 inter, int flag)
+
+void				pillar_virtual_move(t_arch *arch, t_fvct2 inter, int flag)
 {
-	double		percent_tmp;
-	double		*percent;
-	t_fvct2		*pillar;
+	double			percent_tmp;
+	double			*percent;
+	t_fvct2			*pillar;
 
 	if (flag == PILLAR)
 	{
@@ -89,9 +91,12 @@ void			pillar_virtual_move(t_arch *arch, t_fvct2 inter, int flag)
 		pillar = &arch->next;
 	}
 	if (arch->pillar.x == arch->next.x)
-		percent_tmp = (inter.y - arch->pillar.y) / (arch->next.y - arch->pillar.y);
+		percent_tmp = (inter.y - arch->pillar.y)
+			/ (arch->next.y - arch->pillar.y);
 	else
-		percent_tmp = (inter.x - arch->pillar.x) / (arch->next.x - arch->pillar.x);
-	*percent = percent_tmp * (arch->shift_txtr.y - arch->shift_txtr.x) + arch->shift_txtr.x;
+		percent_tmp = (inter.x - arch->pillar.x)
+			/ (arch->next.x - arch->pillar.x);
+	*percent = percent_tmp * (arch->shift_txtr.y - arch->shift_txtr.x)
+	+ arch->shift_txtr.x;
 	*pillar = inter;
 }
